37d9f66058e3172ca82f6716de7c853dc1bb33ba
amamiyasiren <srn59937@gmail.com>
2026-08-16 14:20:48 -0800

    static

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
 
 let notes = [
   {
@@ -22,7 +24,7 @@ let notes = [
   },
 ]
 
-app.use(express.json())
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
